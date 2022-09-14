71486595462c6b9267b4040bf68e01e712814224
11645 <ian.rovner@vtsd.com>
2022-09-14 09:21:21 -0400

    100pt.

---
+++ Lab02vst100.java
@@ -0,0 +1,28 @@
+/*
+    Welcome to your first lab!  All work should be completed inside of this file.
+    Please make sure that as you work on your lab you save, commit, and push everyday.
+
+    As always, if you have any questions reach out and email me!
+
+*/
+
+  public class Lab02vst100 {
+
+    public static void main(String []args)
+    {
+      System.out.println("Lab 02, 100 Point Version ");
+      System.out.println();
+      System.out.println();
+      System.out.print("Measure Today's Knowledge");
+      System.out.println("By yesterday's confusion.");
+      System.out.println();
+      System.out.println("Bewilderment + Exposure = Obvious");
+      System.out.println();
+      System.out.println("# ##### #####   ##### ##### # #   # ##### #####");
+      System.out.println("# #   # #   #   #   # #   # # ##  #   #   #");
+      System.out.println("# #   # #   #   ##### #   # # # # #   #   #####");
+      System.out.println("# #   # #   #   #     #   # # #  ##   #       #");
+      System.out.println("# ##### #####   #     ##### # #   #   #   #####");
+
+    }
+  }
